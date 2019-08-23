0e32f15468e4679f168b08612a0af26f8a5d333b
NAMER <bitagirov@gmil.com>
2019-08-23 18:06:31 +0300

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, {Component} from "react";
+import {BrowserRouter as Router , Route , Switch} from "react-router-dom";
+
 
 class App extends  Component {
   render () {
     return (
       <React.Fragment>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route   path="About" component={About} />
+            <Route   path="Contact" component={Contact} />
+            <Route    component={NoMath} />
 
+          </Switch>
+        </Router>
       </React.Fragment>
     );
   }
 }
 
-export default App;
+export default App; 
