ec594081db779139b3cb636bbe5eacb9bc488a58
NAMER <bitagirov@gmil.com>
2019-08-27 10:26:28 +0300

    add container

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import {Home} from './Home';
 import {About} from './About';
 import {Contact} from './Contact';
 import { NoMatch } from './NoMatch';
+import { Layout } from './components/Layout';
 
 class App extends  Component {
   render () {
     return (
       <React.Fragment>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route   path="About" component={About} />
-            <Route   path="Contact" component={Contact} />
-            <Route    component={NoMatch} />
+          <Layout>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route   path="About" component={About} />
+              <Route   path="Contact" component={Contact} />
+              <Route    component={NoMatch} />
 
-          </Switch>
-        </Router>
+            </Switch>
+          </Router>
+          </Layout>
+          
       </React.Fragment>
     );
   }
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const  Home = () => (
+        <div>
+            <h2> Hello world </h2>
+            <p> meow meow </p>
+            <p> meow meow meow meow </p>
+        </div>
+    )
+
