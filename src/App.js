0bd20c11fe41cca9648ac37593575b8495e0089c
NAMER <bitagirov@gmil.com>
2019-08-30 11:10:58 +0300

    npm istyled-components

--- src/App.js
+++ src/App.js
@@ -5,23 +5,23 @@ import {About} from './About';
 import {Contact} from './Contact';
 import { NoMatch } from './NoMatch';
 import { Layout } from './components/Layout';
+import { NavigationBar} from './components/NavigationBar';
 
-class App extends  Component {
-  render () {
+class App extends Component {
+  render() {
     return (
       <React.Fragment>
+        <Router>
+          <NavigationBar />
           <Layout>
-          <Router>
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route   path="About" component={About} />
-              <Route   path="Contact" component={Contact} />
-              <Route    component={NoMatch} />
-
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route component={NoMatch} />
             </Switch>
-          </Router>
           </Layout>
-          
+        </Router>
       </React.Fragment>
     );
   }
